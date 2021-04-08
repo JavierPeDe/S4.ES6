8f1ff7297e3b3ca76af92ce0537f59d69c20006d
javierPerez <javip107@gmail.com>
2021-04-08 11:01:44 +0200

    Exercici 2

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-//Nivel-1
-//E-1
-const multiply = (num1, num2) => {
-    return num1 * num2;
-}
 
-const toCelsius = (fahrenheit) => {
-    return (5 / 9) * (fahrenheit - 32);
-}
+//exercici 2
+const users = [
+    { firstName: 'Homer', lastName: 'Simpson' },
+    { firstName: 'Marge', lastName: 'Simpson' },
+    { firstName: 'Bart', lastName: 'Simpson' },
+    { firstName: 'Lisa', lastName: 'Simpson' },
+    { firstName: 'Maggie', lastName: 'Simpson' }
+];
 
-const padZeros = (num, totalLen) => {
-    var numStr = num.toString();
-    var numZeros = totalLen - numStr.length;
-    for (var i = 1; i <= numZeros; i++) {
-        numStr = `0${numStr}`;
-    }
-    return numStr;
-}
+const usersFirstName = users.map((user) => {
+    return user.firstName;
+});
 
-const power = (base, exponent) => {
-    var result = 1;
-    for (var i = 0; i < exponent; i++) {
-        result *= base;
-    }
-    return result;
-}
-
-const greet = (who) => {
-    console.log(`Hello ${who}`)
-}
-
-console.log(padZeros(2, 10))
-console.log(power(2, 10))
+console.log(usersFirstName);
