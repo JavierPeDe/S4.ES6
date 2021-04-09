68241d2ad7c0f54f20f05b16fc6ae2e45b18ae68
javierPerez <javip107@gmail.com>
2021-04-09 12:16:31 +0200

    N2-Exercici 7

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
-// Exercici 6
-// Tenim un array de tasques i volem aconseguir un array que contingui únicament els noms de les tasques. 
+// Exercici 7 Nivell 2
+// Realitzar un programa en ES6, amb al menys una funció.
+// Aquest programa demana a l'usuari dos nombres de l'1 a l'10 (con promp es suficient). 
+// Un cop introduïts, es mostra per consola la taula de multiplicar del número 1 que va fins al número 2.
 
-// - Usar .forEach() per obtenir aquest array.
+// Per exemple, si l'usuari introdueix 3 i 5:
 
-// - Usar .map() per obtenir aquest array.
+// 3 X 1 = 3
+// 3 X 2 = 6
+// 3 X 3 = 9
+// 3 X 4 = 12
+// 3 X 5 = 15
 
-var arrayF = [];
-var tasks = [
-    {
-        'name': 'Start React web',
-        'duration': 120
-    },
-    {
-        'name': 'Work out',
-        'duration': 60
-    },
-    {
-        'name': 'Procrastinate on facebook',
-        'duration': 240
+let num1 = prompt('Introdueix el primer numero:');
+let num2 = prompt('Introdueix el segon numero:');
+
+const taulaMultiplicar = (num1, num2) => {
+    let resultat = '';
+
+    for (let i = 1; i <= num2; i++) {
+        resultat += `${num1} X ${i} = ${num1 * i} <br>`
     }
-];
-tasks.forEach(task => {
-    arrayF.push(task.name);
-    console.log(task.name);
-});
-console.log(arrayF);
-
-let arrayMap = tasks.map(task => {
-    return (task.name);
-})
-console.log(arrayMap);
+    document.getElementById("resultat2").innerHTML = resultat;
+}
+taulaMultiplicar(num1, num2);
