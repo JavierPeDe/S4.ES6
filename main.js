61340521fcaf8f56721c40086bbc934db4b96c54
javierPerez <javip107@gmail.com>
2021-04-13 12:42:24 +0200

    Exercici 9 (Nivel-3)

--- main.js
+++ main.js
@@ -1,64 +1,43 @@
-// Exercici 8
+//Exercici 9 (Nivel-3)
+//  Aquest exercici tracta de crear un cercador. Donada una llista de noms, a l'introduir un nom al cercador només s'han de mostrar aquells que coincideixin.
+// Seria desitjable que vagi filtrant conforme es va omplint el nom. També tenir en compte maquetar la solució final perquè tingui bon aspecte.
 
-// Modificar l'exercici anterior perquè els números s'introdueixin a través d'un formulari (amb validacions), 
-// i que la taula de multiplicar la mostri en una taula HTML
+// Code snippet:
 
-// let num1 = prompt('Introdueix el primer numero:');
-// let num2 = prompt('Introdueix el segon numero:');
+// <input type="text" id="myInput" onkeyup="filterNames()" placeholder="Buscar nom.." title="Buscar nom">
 
-function validacion() {
-    let error = 0;
-    var inputNum1 = document.forms["form"]["inputN1"];
-    var inputNum2 = document.forms["form"]["inputN2"];
+// <ul id="myUL">
+//   <li><a href="#">Albert</a></li>
+//   <li><a href="#">Ismael</a></li>
+//   <li><a href="#">Luis</a></li>
+//   <li><a href="#">Rubén</a></li>
+//   <li><a href="#">Jonatan</a></li>
+//   <li><a href="#">Jake</a></li>
+//   <li><a href="#">Jose</a></li>
+// </ul>
 
-    form.classList.remove("is-invalid");
-    form.classList.remove("is-valid");
-
-
-    //Validacio Primer numero
-    if (inputNum1.value == "") {
-        inputNum1.classList.add("is-invalid");
-        document.getElementById("eN1").textContent = "Introdueix un número";
-        error++;
-    }
-    else if (parseInt(inputNum1.value) < 1 || parseInt(inputNum1.value) >= 10) {
-        inputNum1.classList.add("is-invalid");
-        document.getElementById("eN1").textContent = "Prova amb un número del 1-10";
-        error++;
-    }
-    else {
-        inputNum1.classList.add("is-valid");
-        document.getElementById("vN1").textContent = "Número aceptat";
-    }
-    //Validacio Segon numero
-    if (inputNum2.value == "") {
-        inputNum2.classList.add("is-invalid");
-        document.getElementById("eN2").textContent = "Introdueix un número";
-        error++;
-    }
-    else if (parseInt(inputNum2.value) < 1 || parseInt(inputNum2.value) > 10) {
-        inputNum2.classList.add("is-invalid");
-        document.getElementById("eN2").textContent = "Prova amb un número del 1-10";
-        error++;
-    }
-    else {
-        inputNum2.classList.add("is-valid");
-        document.getElementById("vN2").textContent = "Número aceptat";
-    }
-    if (error == 0) { taulaMultiplicar(inputNum1.value, inputNum2.value) } //Cridem la funcio taulamultiplicar si no hi ha errors
-    // return (error == 0); 
+var lista = [] //guardamos la lista original
+for (const nombres of document.querySelectorAll("a")) {
+  lista.push(nombres.innerHTML);
 }
+var listaTemporal = [...lista];
+function filterNames(event) {
+  let input = event.value.toLowerCase();
+  //Devuelve de la lista solamente las coincidencias a listaTemporal
+  listaTemporal = lista.filter(function (n) {
+    return n.toLowerCase().indexOf(input) > -1;
+  });
 
-const taulaMultiplicar = (num1, num2) => {
-    let resultat = '';
+  console.log(listaTemporal); //iformativo
 
-    for (let i = 1; i <= num2; i++) {
-        resultat += `${num1} X ${i} = ${num1 * i} <br>`
-    }
-    document.getElementById("resultat_P").innerHTML = resultat;
+  var myList = document.getElementById('myUL');
+  myList.innerHTML = ''; //Borramos la lista de HTML y la volvemos a rellenar con las coincidencias en cada evento.
+  for (let newName of listaTemporal) {
+    var itemLi = document.createElement('li');
+    var a = document.createElement('a');
+    a.textContent = newName;
+    a.setAttribute('href', '#');
+    itemLi.appendChild(a);
+    document.getElementById('myUL').appendChild(itemLi);
+  }
 }
-form.addEventListener('blur', (event) => {
-    console.log(event);
-    if (event.target.value != '') event.target.classList.remove('is-invalid');
-    if (event.target.value != '') event.target.classList.remove('is-valid');
-}, true);
